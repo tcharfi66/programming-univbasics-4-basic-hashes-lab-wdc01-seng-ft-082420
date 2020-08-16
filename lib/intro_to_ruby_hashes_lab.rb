eb2146188407b99f34d517309f0cdf8c8b3fe6b8
Tej Charfi <charfi@usc.edu>
2020-08-16 19:00:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,12 +5,12 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  {:name="Tej Charfi", :job="Software Engineer"}
+  {:name=>"Tej Charfi", :job=>"Software Engineer"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  {:name="Grace Hopper"}
+  {:name=>"Grace Hopper"}
 end
 
 
